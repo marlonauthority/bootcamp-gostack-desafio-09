36229a729344e272b343787528e88fd967f76d2d
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-09-13 19:37:21 -0400

    Utilizando o Rocket/Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import Logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    //
+  }
   return (
     <>
       <img src={Logo} alt="Meetapp" />
 
-      <form>
-        <input type="email" placeholder="Digite seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Entrar</button>
-      </form>
+      </Form>
       <Link to="/register">Criar conta grátis</Link>
     </>
   );
