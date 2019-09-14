25a3b3fcc764ffdc0b190988ed0ed1eb8550e565
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-09-14 15:45:47 -0400

    Autenticacao p.2

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,13 +5,15 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
   // -> logado?
-  const signed = false;
+  const { signed } = store.getState().auth;
   // -> Não esta logado e a rota é privada
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
