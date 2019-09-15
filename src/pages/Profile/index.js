0e93029a5bc0d07b9d72f9a05a2a657152183896
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-09-15 14:57:13 -0400

    update profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import { MdAddCircleOutline } from 'react-icons/md';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit() {
-    //
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
   }
 
   return (
