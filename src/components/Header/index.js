de2993de548bb235bc7457787fda04d52de719f9
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-09-15 15:05:02 -0400

    Dados no Header e Logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Logo from '~/assets/logo.svg';
 
+import { signOut } from '~/store/modules/auth/actions';
+
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -17,10 +27,12 @@ export default function Header() {
 
         <Profile>
           <div>
-            <strong>Marlon Authority</strong>
+            <strong>{profile.name}</strong>
             <Link to="/profile">Meu Perfil</Link>
           </div>
-          <button type="button">Sair</button>
+          <button type="button" onClick={handleSignOut}>
+            Sair
+          </button>
         </Profile>
       </Content>
     </Container>
